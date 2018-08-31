8c63cc61d64f8952e7153b0303d69b3ffc4a5b31
Dashmeet Singh <35026920+dashmeet-hu@users.noreply.github.com>
2018-08-31 18:59:57 +0530

    Update start_node_application.sh

--- codeDeployScripts/start_node_application.sh
+++ codeDeployScripts/start_node_application.sh
@@ -2,16 +2,17 @@
 
 #sudo su
 cd /var/www/html/ustraa
+
+sudo chmod -R 777 node_modules
+sudo chmod -R 777 package-lock.json
+sudo chmod -R 777 yarn.lock
+
 rm -rf node_modules
 rm -rf package-lock.json
 rm -rf yarn.lock
 
 #nvm use 8.11.3
 npm install
-
-chmod -R 777 node_modules
-chmod -R 777 package-lock.json
-chmod -R 777 yarn.lock
 
 echo $USER
 ls
