7c827235b464be7d6d662d6236b71c643adb6894
Dashmeet Singh <35026920+dashmeet-hu@users.noreply.github.com>
2018-08-31 17:01:23 +0530

    Update start_node_application.sh

--- codeDeployScripts/start_node_application.sh
+++ codeDeployScripts/start_node_application.sh
@@ -2,15 +2,13 @@
 
 #sudo su
 cd /var/www/html/ustraa
-chmod -R 777 .
 
 rm -rf node_modules
 rm -rf package-lock.json
-#rm -rf yarn.lock
+rm -rf yarn.lock
 
 #nvm use 8.11.3
-/home/ubuntu/.nvm/versions/node/v8.11.3/bin/npm install
-#npm install
+npm install
 
 pm2 start ustraa
 #exit
