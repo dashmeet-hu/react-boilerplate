523a70f7c9826660b4649597786039d54a7e7d49
Dashmeet Singh <35026920+dashmeet-hu@users.noreply.github.com>
2018-09-04 16:45:38 +0530

    Update before_start_node_application.sh

--- codeDeployScripts/before_start_node_application.sh
+++ codeDeployScripts/before_start_node_application.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#sudo su
+cd /var/www/html/ustraa
+
+#chmod -R 777 node_modules
+#chmod -R 777 package-lock.json
+#chmod -R 777 yarn.lock
+
+#rm -rf node_modules
+rm -rf package-lock.json
+rm -rf yarn.lock
+
+#nvm use 8.11.3
+npm install
+
+#pm2 start ustraa
+#exit
